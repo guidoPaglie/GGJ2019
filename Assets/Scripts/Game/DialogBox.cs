b8b4b2ce71de1a64ffaae5786b428a294f81f424
Martin Gonzalez <martin.gonzalez@etermax.com>
2019-01-26 13:47:10 -0300

    Add dialog "System"

---
+++ Assets/Scripts/Game/DialogBox.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class DialogBox : MonoBehaviour {
+    public Animator animator;
+    public TextMeshPro text;
+    public float dialogEnableTime;
+    private bool _dialogEnabled;
+    private float _timer;
+    
+    public void ShowMessage(string message) {
+        animator.SetTrigger("In");
+        _timer = 0;
+        text.text = "";
+        _dialogEnabled = true;
+        StartCoroutine(ShowLetters(message));
+    }
+
+    private IEnumerator ShowLetters(string message) {
+        yield return new WaitForSeconds(0.5f);
+                
+        foreach (var letter in message) {
+            text.text += letter;
+            yield return null;
+        }
+    }
+
+    private void Start() {
+        transform.localScale = new Vector3(0,0,0);
+    }
+
+    void Update() {
+        if (_dialogEnabled) {
+            if (_timer < dialogEnableTime) {
+                _timer += Time.deltaTime;
+            }
+            else {
+                _timer = 0;
+                _dialogEnabled = false;
+                HideDialog();
+            }
+        }
+    }
+
+    private void HideDialog() {
+        animator.SetTrigger("Out");    
+    }
+}
