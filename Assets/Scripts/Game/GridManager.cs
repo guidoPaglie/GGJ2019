77388fe790bea2c9f891ea53f1cb5620089f6e7c
Guido Pagliettini <guido_pagliettini@hotmail.com>
2019-01-26 01:20:28 -0300

    Grid first version

---
+++ Assets/Scripts/Game/GridManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    private Cell[,] grid = new Cell[60,30];
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = 0; x < 60; x++)
+        {
+            for (int y = 0; y < 30; y++)
+            {
+                
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
