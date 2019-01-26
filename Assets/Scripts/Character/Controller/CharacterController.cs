e10e23af22cb6c01ca298ebeba3c3f8cea9e26e4
Martin Gonzalez <martin.gonzalez@etermax.com>
2019-01-26 01:05:16 -0300

    Add Character MoveTest

---
+++ Assets/Scripts/Character/Controller/CharacterController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using UnityEngine;
+
+public class CharacterController : UnityXboxController {
+	[Range(1, 32)] public int cellSize;
+	public SpriteRenderer CharacterSpriteRenderer;
+	public Sprite CharacterUpSprite;
+	public Sprite CharacterDownSprite;
+	public Sprite CharacterLeftSprite;
+	public Sprite CharacterRightSprite;
+	private Direction _currentDirection = Direction.Up;
+
+	private bool _isJumping;
+
+	void Awake() {
+		UpdateSprite(_currentDirection);
+	}
+
+	protected override void OnAPressed() {
+		Debug.Log("A Pressed");
+	}
+
+	protected override void OnBPressed() {
+		Debug.Log("B Pressed");
+	}
+
+	protected override void OnYPressed() {
+		Debug.Log("Y Pressed");
+	}
+
+	protected override void OnXPressed() {
+		Debug.Log("X Pressed");
+	}
+
+	protected override void OnLeftStickLeft() {
+		MoveCharacter(Direction.Left);
+	}
+
+	protected override void OnLeftStickRight() {
+		MoveCharacter(Direction.Right);
+	}
+
+	protected override void OnLeftStickUp() {
+		MoveCharacter(Direction.Up);
+	}
+
+	protected override void OnLeftStickDown() {
+		MoveCharacter(Direction.Down);
+	}
+
+	void MoveCharacter(Direction direction) {
+		switch (direction) {
+			case Direction.Up:
+				Move(Direction.Up);
+				
+				break;
+			case Direction.Down:
+				Move(Direction.Down);
+				break;
+			case Direction.Left:
+				Move(Direction.Left);
+				break;
+			case Direction.Right:
+				Move(Direction.Right);
+				break;
+			default:
+				throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+		}
+		UpdateSprite(direction);
+	}
+
+	void UpdateSprite(Direction direction) {
+		switch (direction) {
+			case Direction.Up:
+				CharacterSpriteRenderer.sprite = CharacterUpSprite;
+				break;
+			case Direction.Down:
+				CharacterSpriteRenderer.sprite = CharacterDownSprite;
+				break;
+			case Direction.Left:
+				CharacterSpriteRenderer.sprite = CharacterLeftSprite;
+				break;
+			case Direction.Right:
+				CharacterSpriteRenderer.sprite = CharacterRightSprite;
+				break;
+			default:
+				throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+		}
+	}
+
+	void Move(Direction direction) {
+		_currentDirection = direction;
+		int x;
+		int y;		
+		var transform1 = transform;
+		var position = transform1.position;
+		
+		switch (direction) {
+			case Direction.Up:
+				x = 0;
+				y = 1;
+				break;
+			case Direction.Down:
+				x = 0;
+				y = -1;
+				break;
+			case Direction.Left:
+				x = -1;
+				y = 0;
+				break;
+			case Direction.Right:
+				x = 1;
+				y = 0;
+				break;
+			default:
+				throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+		}
+		position = new Vector2(position.x + cellSize * x, position.y + cellSize * y);
+		transform1.position = position;
+	}
+
+	protected override void OnLeftYAxisMove(float yAxis) {
+	}
+
+	protected override void OnRightXAxisMove(float xAxis) {
+	}
+
+	protected override void OnRightYAxisMove(float yAxis) {
+	}
+
+	private enum Direction {
+		Up,
+		Down,
+		Left,
+		Right
+	}
+}
---
+++ Assets/Scripts/Xbox/UnityXboxController.cs
@@ -0,0 +1,149 @@
+﻿using UnityEngine;
+using XboxCtrlrInput;
+
+public class UnityXboxController : MonoBehaviour {
+	public XboxController controller;
+
+	private bool _leftStickLeft;
+	private bool _leftStickRight;
+	private bool _leftStickUp;
+	private bool _leftStickDown;
+
+	void FixedUpdate() {
+		if (XCI.GetButtonDown(XboxButton.A, controller)) {
+			OnAPressed();
+		}
+
+		if (XCI.GetButtonDown(XboxButton.B, controller)) {
+			OnBPressed();
+		}
+
+		if (XCI.GetButtonDown(XboxButton.X, controller)) {
+			OnXPressed();
+		}
+
+		if (XCI.GetButtonDown(XboxButton.Y, controller)) {
+			OnYPressed();
+		}
+
+		if (XCI.GetButton(XboxButton.A, controller)) {
+			OnAHold();
+		}
+
+		if (XCI.GetButton(XboxButton.B, controller)) {
+			OnBHold();
+		}
+
+		if (XCI.GetButton(XboxButton.X, controller)) {
+			OnXHold();
+		}
+
+		if (XCI.GetButton(XboxButton.Y, controller)) {
+			OnYHold();
+		}
+
+		OnLeftXAxisMove(XCI.GetAxis(XboxAxis.LeftStickX, controller));
+		OnLeftYAxisMove(XCI.GetAxis(XboxAxis.LeftStickY, controller));
+
+		OnRightXAxisMove(XCI.GetAxis(XboxAxis.RightStickX, controller));
+		OnRightYAxisMove(XCI.GetAxis(XboxAxis.RightStickY, controller));
+
+		if (!_leftStickRight) {
+			if (XCI.GetAxis(XboxAxis.LeftStickX, controller) >= 1) {
+				OnLeftStickRight();
+				_leftStickRight = true;
+			}
+		}
+		else {
+			if (XCI.GetAxis(XboxAxis.LeftStickX, controller) < 0.5f) {
+				_leftStickRight = false;
+			}
+		}
+
+		if (!_leftStickLeft) {
+			if (XCI.GetAxis(XboxAxis.LeftStickX, controller) <= -1) {
+				OnLeftStickLeft();
+				_leftStickLeft = true;
+			}
+		}
+		else {
+			if (XCI.GetAxis(XboxAxis.LeftStickX, controller) > -0.5f) {
+				_leftStickLeft = false;
+			}
+		}
+		
+		if (!_leftStickUp) {
+			if (XCI.GetAxis(XboxAxis.LeftStickY, controller) >= 1) {
+				OnLeftStickUp();
+				_leftStickUp = true;
+			}
+		}
+		else {
+			if (XCI.GetAxis(XboxAxis.LeftStickY, controller) < 0.5f) {
+				_leftStickUp = false;
+			}
+		}
+
+		if (!_leftStickDown) {
+			if (XCI.GetAxis(XboxAxis.LeftStickY, controller) <= -1) {
+				OnLeftStickDown();
+				_leftStickDown = true;
+			}
+		}
+		else {
+			if (XCI.GetAxis(XboxAxis.LeftStickY, controller) > -0.5f) {
+				_leftStickDown = false;
+			}
+		}
+	}
+
+	protected virtual void OnLeftStickDown() {
+		
+	}
+
+	protected virtual void OnLeftStickUp() {
+		
+	}
+
+	protected virtual void OnLeftStickRight() {
+	}
+
+	protected virtual void OnLeftStickLeft() {
+	}
+
+	protected virtual void OnRightXAxisMove(float xAxis) {
+	}
+
+	protected virtual void OnRightYAxisMove(float yAxis) {
+	}
+
+	protected virtual void OnLeftXAxisMove(float xAxis) {
+	}
+
+	protected virtual void OnLeftYAxisMove(float yAxis) {
+	}
+
+	protected virtual void OnAPressed() {
+	}
+
+	protected virtual void OnBPressed() {
+	}
+
+	protected virtual void OnXPressed() {
+	}
+
+	protected virtual void OnYPressed() {
+	}
+
+	protected virtual void OnAHold() {
+	}
+
+	protected virtual void OnBHold() {
+	}
+
+	protected virtual void OnYHold() {
+	}
+
+	protected virtual void OnXHold() {
+	}
+}
