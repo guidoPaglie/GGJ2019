1e76e6dc9050e2d14865f47d63999826b3950a1b
Martin Gonzalez <martin.gonzalez@etermax.com>
2019-01-27 02:55:11 -0300

    TvNoise Effect

---
+++ Assets/Scripts/Game/TvNoise.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TvNoise : MonoBehaviour {
+   public SpriteRenderer SpriteRenderer;
+
+   public float tickTime;
+   private float _timer;
+   void Update() {
+      if (_timer < tickTime) {
+         _timer += Time.deltaTime;       
+      }
+      else {
+         _timer = 0;
+         SpriteRenderer.flipX = !SpriteRenderer.flipX;
+      }
+   }
+}
