2ca2f53a1d1c0bb0a2bd5d95945df3b1a0d43a7f
Guido Pagliettini <guido_pagliettini@hotmail.com>
2019-01-27 15:06:58 -0300

    turn off effect

---
+++ Assets/Scripts/TurnOffEvent.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class TurnOffEvent : MonoBehaviour
+{
+    public void PlaySound()
+    {
+        AudioManager.Instance.PlaySound("tv_off");
+        AudioManager.Instance.PauseMusic();
+    }
+}
